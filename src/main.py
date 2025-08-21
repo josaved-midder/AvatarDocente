0bbe57e2d21eaf76d7c938d1f2defb72b81a203e
Juli1993cl <ijulianagallego@gmail.com>
2025-08-21 10:38:05 -0500

    eleminar archivos prueba

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from .gui.windows.ventana_principal import VentanaPrincipal
+from .gui.windows.ventanaPrincipal import Ui_ventanaPrincipal
 
 
 def main():
     app = QApplication(sys.argv)
-    w = VentanaPrincipal()
+    w = Ui_ventanaPrincipal()
     w.show()
     sys.exit(app.exec_())
 
